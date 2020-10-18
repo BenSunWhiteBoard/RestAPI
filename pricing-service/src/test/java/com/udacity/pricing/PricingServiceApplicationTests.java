47bc6481142fdad629b3b2774d4667f70595b954
BenSun <amortizedanalysis@gmail.com>
2020-10-18 22:32:11 +0200

    finish carController and API documentation using Swagger

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,79 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads(){
+    }
+
+    @Test
+    public void eurekaReady() throws Exception {
+        mockMvc.perform(get("localhost:" + 8082 + "/services/price?vehicleId=1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getPrice() {
+        //happy path
+        ResponseEntity<Price> responseEntity =
+                restTemplate.getForEntity("http://192.168.31.55:" + port + "/services/price?vehicleId=1",
+                        Price.class);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+
+        responseEntity =
+                restTemplate.getForEntity("http://192.168.31.55:" + port + "/services/price?vehicleId=19",
+                        Price.class);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+
+    }
+
+    @Test
+    public void getPriceBadRequest() {
+        //bad request
+        ResponseEntity<Price> responseEntity =
+                restTemplate.getForEntity("http://192.168.31.55:" + port + "/services/price?vehicleId",
+                        Price.class);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    public void getPriceException() {
+        //exception
+        ResponseEntity<Price> responseEntity =
+                restTemplate.getForEntity("http://192.168.31.55:" + port + "/services/price?vehicleId=24",
+                        Price.class);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+
+    }
 
 }
--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -1,30 +1,46 @@
 package com.udacity.vehicles.service;
 
+import com.udacity.vehicles.client.maps.Address;
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.Price;
+import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
- * Implements the car service create, read, update or delete
- * information about vehicles, as well as gather related
- * location and price data when desired.
+ * Implements the car service create, read, update or delete information about vehicles, as well as
+ * gather related location and price data when desired.
  */
 @Service
 public class CarService {
 
     private final CarRepository repository;
+    private final WebClient mapsClient;
+    private final WebClient pricingClient;
 
-    public CarService(CarRepository repository) {
+    public CarService(
+            CarRepository repository,
+            @Qualifier("maps") @Autowired WebClient mapsClient,
+            @Qualifier("pricing") @Autowired WebClient pricingClient) {
         /**
-         * TODO: Add the Maps and Pricing Web Clients you create
+         *   Add the Maps and Pricing Web Clients you create
          *   in `VehiclesApiApplication` as arguments and set them here.
          */
         this.repository = repository;
+        this.mapsClient = mapsClient;
+        this.pricingClient = pricingClient;
+
     }
 
     /**
      * Gathers a list of all vehicles
+     *
      * @return a list of all vehicles in the CarRepository
      */
     public List<Car> list() {
@@ -33,41 +49,61 @@ public class CarService {
 
     /**
      * Gets car information by ID (or throws exception if non-existent)
+     *
      * @param id the ID number of the car to gather information on
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) {
         /**
-         * TODO: Find the car by ID from the `repository` if it exists.
+         *   Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          *   Remove the below code as part of your implementation.
          */
-        Car car = new Car();
+        Optional<Car> carResult = repository.findById(id);
+        if (!carResult.isPresent()) {
+            throw new CarNotFoundException("car No." + id + "not exists");
+        }
+        Car car = carResult.get();
 
         /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
+         * Use the Pricing Web client you create in `VehiclesApiApplication`
          *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
+         * Set the price of the car
          * Note: The car class file uses @transient, meaning you will need to call
          *   the pricing service each time to get the price.
          */
-
-
+        Price price = pricingClient.get()
+                .uri("/services/price?vehicleId=" + car.getId())
+                .retrieve()
+                .bodyToMono(Price.class)
+                .block();
+        car.setPrice(price.getPrice().toString());
         /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
+         * Use the Maps Web client you create in `VehiclesApiApplication`
          *   to get the address for the vehicle. You should access the location
          *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
+         * Set the location of the vehicle, including the address information
          * Note: The Location class file also uses @transient for the address,
          * meaning the Maps service needs to be called each time for the address.
          */
-
+        Double lat = car.getLocation().getLat();
+        Double lon = car.getLocation().getLon();
+        Address address= mapsClient.get()
+                .uri("/maps?lat="+lat+"&lon="+lon)
+                .retrieve()
+                .bodyToMono(Address.class)
+                .block();
+        car.getLocation().setAddress(address.getAddress());
+        car.getLocation().setCity(address.getCity());
+        car.getLocation().setState(address.getState());
+        car.getLocation().setZip(address.getZip());
 
         return car;
     }
 
     /**
      * Either creates or updates a vehicle, based on prior existence of car
+     *
      * @param car A car object, which can be either new or existing
      * @return the new/updated car is stored in the repository
      */
@@ -80,25 +116,29 @@ public class CarService {
                         return repository.save(carToBeUpdated);
                     }).orElseThrow(CarNotFoundException::new);
         }
-
+        System.out.println(car);
         return repository.save(car);
     }
 
     /**
      * Deletes a given car by ID
+     *
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
         /**
-         * TODO: Find the car by ID from the `repository` if it exists.
+         *   Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          */
-
+        Optional<Car> car = repository.findById(id);
+        if (!car.isPresent()) {
+            throw new CarNotFoundException("car No." + id + "not exists");
+        }
 
         /**
-         * TODO: Delete the car from the repository.
+         *   Delete the car from the repository.
          */
-
+        repository.deleteById(id);
 
     }
 }
